b51feabe5da16a837eced78c9bbc9c5c4e07196f
Peturs94 <Petursson94@gmail.com>
2020-10-27 14:58:13 +0000

    banner test

--- bloghub/src/components/Banner.js
+++ bloghub/src/components/Banner.js
@@ -5,6 +5,7 @@ import './Banner.css';
 function Banner() {
     return (
         <div className='Banner-container'>
+            <video src='/videos/video-1.mp4' autoPlay loop muted />
             <h1>Blog-hub container</h1>
         </div>
     )
